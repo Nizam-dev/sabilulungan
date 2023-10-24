2751d3011a697db28a9e4a0fff1f89dc72467030
Nizam <mahfudzkhoirunnizam@gmail.com>
2023-10-24 10:00:51 +0700

    update template

---
+++ app/Views/template/css.php
@@ -0,0 +1,4 @@
+<link rel="icon" type="image/png" href="<?php echo base_url('public/template/img/favicon.png') ?>">
+<link rel="stylesheet" href="<?php echo base_url('public/template/css/main.css') ?>">
+<link rel="stylesheet" href="<?php echo base_url('public/template/css/custom.css') ?>">
+<script src="<?php echo base_url('public/template/js/vendor/modernizr-2.6.2.min.js') ?>"></script>
---
+++ app/Views/template/js.php
@@ -0,0 +1,12 @@
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="<?php echo base_url('public/template/js/vendor/jquery-1.10.2.min.js') ?>"></script>
+<script src="<?php echo base_url('public/template/js/vendor/jquery.fancybox.pack.js') ?>"></script>
+<script src="<?php echo base_url('public/template/js/vendor/zebra_datepicker.js') ?>"></script>
+
+<script src="<?php echo base_url('public/template/js/main.js') ?>"></script>
+
+<script type="text/javascript" src="<?php echo base_url('public/template/js/jquery.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('public/template/js/form.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('public/template/js/popup.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('public/template/js/misc.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('public/template/js/editor.js') ?>"></script>
